8dac63de3c6357cbeef3791db1433d3d05ced172
camilopinheiro-clina <camilo.pinheiro@clina.care>
2022-09-05 17:33:00 -0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'redirecionador';
+  hostname: any;
+  origin: any;
+
+  constructor() {
+    const {
+      hostname, origin
+    } = new URL('https://dnyaneshtechno.sharepoint.com')
+
+    const [subdomain] = hostname.split('.')
+
+  }
 }
