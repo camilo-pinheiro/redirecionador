ac9811c48892454901f50f543ad2def26c99d9c8
camilopinheiro-clina <camilo.pinheiro@clina.care>
2022-09-05 18:02:40 -0300

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,13 +10,8 @@ export class AppComponent {
   title = 'redirecionador';
   constructor(@Inject(DOCUMENT) private document: Document) {
     console.log(this.document.location)
-    const url = `https://${this.document.location.host.split('.')[1]}.
-    ${this.document.location.host.split('.')[2]}/
-    ${this.document.location.host.split('.')[0]}
-    ${this.document.location.pathname}
-    ${this.document.location.search}`
+    const url = `https://${this.document.location.host.split('.')[1]}.${this.document.location.host.split('.')[2]}/${this.document.location.host.split('.')[0]}${this.document.location.pathname}${this.document.location.search}`
     console.log(url);
-    return;
     setTimeout(() => {
       window.location.href = url;
     }, 2000);
